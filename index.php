f69ff060a202cabcb9bfec943d429b0eb5006b4d
HYPERPC <aganbegyanm@gmail.com>
2022-09-04 15:29:19 +0300

    start

---
+++ index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>dzen</title>
+</head>
+<body>
+<div class="content_wraper">
+    <div class="nav">
+        <div class="logo">
+            <a href="https://zen.yandex.ru/media/id/5fb822f01dae9b66f7e4fff5/kak-rybachit-v-gta-5-rp-gaid-po-rybalke-dlia-novichka-5fb822f6ffe1de7f5cef8336"><img src="res/Screenshot_7.png" alt=""></a>
+        </div>
+        <div class="autor">
+            <img src="https://avatars.dzeninfra.ru/get-zen-logos/212539/pub_5fb822f01dae9b66f7e4fff5_5fdcb8801e8e8c460f2063cb/min_xh" alt="">
+            <div class="text">
+                <p class="pod2">GameDealer</p>
+                <p class="pod">7 подписчиков</p>
+            </div>
+            <a href="https://zen.yandex.ru/id/5fb822f01dae9b66f7e4fff5">Подписаться</a>
+            <a href="https://travel.yandex.ru/avia/?clid=2379721-910&utm_source=distribution&utm_medium=bookmark&utm_campaign=ru"><img src="res/d.png" alt=""></a>
+        </div>
+        <div class="searh">
+            <img src="res/sadadsadsa.png" alt="">
+        </div>
+        <div class="login">
+            <a href="https://passport.yandex.ru/registration?origin=zen&retpath=https%3A%2F%2Fzen.yandex.ru%2Fmedia%2Fid%2F5fb822f01dae9b66f7e4fff5%2Fkak-rybachit-v-gta-5-rp-gaid-po-rybalke-dlia-novichka-5fb822f6ffe1de7f5cef8336"><p>Регистрация</p></a>
+            <a href="https://passport.yandex.ru/auth?origin=zen_pubs&retpath=https%3A%2F%2Fzen.yandex.ru%2Fmedia%2Fid%2F5fb822f01dae9b66f7e4fff5%2Fkak-rybachit-v-gta-5-rp-gaid-po-rybalke-dlia-novichka-5fb822f6ffe1de7f5cef8336"><p>Войти</p></a>
+        </div>
+    </div>
+    <div class="main">
+        <div class="left_menu">
+            <a href=""><img src="res/like.png" alt="">20 нравиться</a>
+            <a href=""><img src="res/dis.png" alt="">Не нравится</a>
+            <a href=""><img src="res/com.png" alt="">4 обсудили</a>
+            <a href=""><img src="res/share.png" alt="">Поделиться</a>
+        </div>
+        <div class="content">
+            <h1>Как рыбачить в GTA 5 RP. Гайд по рыбалке для новичка.</h1>
+            <b class="data">20 ноября 2020
+                8,8K прочитали</b>
+            <img class="img_one" src="https://avatars.dzeninfra.ru/get-zen_doc/1706869/pub_5fb822f6ffe1de7f5cef8336_5fb82336ffe1de7f5cefd707/scale_1200" alt="">
+            <p>Если вы только начали играть на серверах GTA 5 RP мы поможем вам с началом рыбалки.</p>
+            <p>Во первых, это самая доступная работа в GTA 5 RP, которую можно начать на первом уровне не считая стройки.</p>
+            <p>Первое, что необходимо это минимальная сумма денег - 3500 удочка + наживка 35 за штуку. И желательно автомобиль - 5000.</p>
+           <div class="lifehack">
+               <p>Лайфхак: удочку можно купить с рук. Можно заехать на рынок или просто спросить у рыбаков на одном из пирсов. С большой доли вероятности вам продадут удочку первого уровня за 1500. Так как, опытные рыбаки, купив новую удочку, хотят избавится от старой.</p>
+           </div>
+            <p>Кончено лучше сначала купить автомобиль с большим багажником. Лучшим вариантом будет Ingot за 5,5 тысяч. он совмещает себе цену и большой багажник 90 кг (вы увезете много рыбы за раз)</p>
+            <img class="img_content" src="https://avatars.dzeninfra.ru/get-zen_doc/3337090/pub_5fb822f6ffe1de7f5cef8336_5fb825dc572b862575b249fa/scale_1200" alt="">
+            <div class="lifehack">
+                <p>Лайфхак: Автомобиль так же можно дешево купить на рынке с рук. Авторынок находится на севере города (розовый значок).</p>
+            </div>
+        </div>
+        <div class="right_menu">
+            <h2>Другие публикации канала</h2>
+            <a href=""><img src="res/town.png" alt=""><p>Как начать играть в Town Star и зарабатывать</p></a>
+            <a href=""><img src="https://avatars.dzeninfra.ru/get-zen_doc/5163854/pub_60f6032ac6cba86dfd85c612_60f605dd8346e576a3420271/smart_crop" alt=""><p>«Бенфика» против «Спартака». Руй Витория против своих.</p></a>
+            <a href=""><img src="https://avatars.dzeninfra.ru/get-zen_doc/5226854/pub_60e79731015e0849c068c909_60e797dd1d503a7bca95953b/smart_crop" alt=""><p>Как начать играть в Town Star и зарабатывать</p></a>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
